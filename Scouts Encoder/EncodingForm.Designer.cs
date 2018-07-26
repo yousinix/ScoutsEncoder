a31319423e0524a78f78db9402ccf1fbf7ec1bb1
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-07-26 18:05:02 +0200

    Basic encoder software
     * 4 codes added
     * Customizable keys for each code
     * Choose characters/words spacing style
     * Simple GUI

---
+++ Scouts Encoder/EncodingForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Scouts_Encoder
+{
+    partial class EncodingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.EncodeButton = new System.Windows.Forms.Button();
+            this.CodesComboBox = new System.Windows.Forms.ComboBox();
+            this.CodeKeyButton = new System.Windows.Forms.Button();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.DashesCheckBox = new System.Windows.Forms.CheckBox();
+            this.KeysComboBox = new System.Windows.Forms.ComboBox();
+            this.SlashesCheckBox = new System.Windows.Forms.CheckBox();
+            this.CharactersSpacingCheckBox = new System.Windows.Forms.CheckBox();
+            this.WordsSpacingCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.AcceptsReturn = true;
+            this.InputTextBox.AllowDrop = true;
+            this.InputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputTextBox.Font = new System.Drawing.Font("Kawkab Mono Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.InputTextBox.Location = new System.Drawing.Point(627, 12);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.InputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.InputTextBox.Size = new System.Drawing.Size(602, 532);
+            this.InputTextBox.TabIndex = 7;
+            this.InputTextBox.Text = "ادخل الشفرة هنا ...";
+            this.InputTextBox.Enter += new System.EventHandler(this.InputTextBox_Enter);
+            this.InputTextBox.Leave += new System.EventHandler(this.InputTextBox_Leave);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.AcceptsReturn = true;
+            this.OutputTextBox.AllowDrop = true;
+            this.OutputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputTextBox.Font = new System.Drawing.Font("Kawkab Mono Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputTextBox.Location = new System.Drawing.Point(17, 12);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.OutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.OutputTextBox.Size = new System.Drawing.Size(602, 532);
+            this.OutputTextBox.TabIndex = 8;
+            // 
+            // EncodeButton
+            // 
+            this.EncodeButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.EncodeButton.Enabled = false;
+            this.EncodeButton.FlatAppearance.BorderSize = 0;
+            this.EncodeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EncodeButton.Font = new System.Drawing.Font("Cairo", 10.2F);
+            this.EncodeButton.Location = new System.Drawing.Point(453, 572);
+            this.EncodeButton.Name = "EncodeButton";
+            this.EncodeButton.Size = new System.Drawing.Size(158, 40);
+            this.EncodeButton.TabIndex = 4;
+            this.EncodeButton.Text = "تشفير";
+            this.EncodeButton.UseVisualStyleBackColor = false;
+            this.EncodeButton.Click += new System.EventHandler(this.EncodeButton_Click);
+            // 
+            // CodesComboBox
+            // 
+            this.CodesComboBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CodesComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CodesComboBox.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CodesComboBox.FormattingEnabled = true;
+            this.CodesComboBox.Items.AddRange(new object[] {
+            "يسوع",
+            "رقمية",
+            "عكسية",
+            "المورس"});
+            this.CodesComboBox.Location = new System.Drawing.Point(999, 572);
+            this.CodesComboBox.Name = "CodesComboBox";
+            this.CodesComboBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.CodesComboBox.Size = new System.Drawing.Size(158, 40);
+            this.CodesComboBox.TabIndex = 1;
+            this.CodesComboBox.Text = "اختر الشفرة";
+            this.CodesComboBox.SelectedIndexChanged += new System.EventHandler(this.CodesComboBox_SelectedIndexChanged);
+            // 
+            // CodeKeyButton
+            // 
+            this.CodeKeyButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CodeKeyButton.Enabled = false;
+            this.CodeKeyButton.FlatAppearance.BorderSize = 0;
+            this.CodeKeyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CodeKeyButton.Font = new System.Drawing.Font("Cairo", 10.2F);
+            this.CodeKeyButton.Location = new System.Drawing.Point(635, 572);
+            this.CodeKeyButton.Name = "CodeKeyButton";
+            this.CodeKeyButton.Size = new System.Drawing.Size(158, 40);
+            this.CodeKeyButton.TabIndex = 3;
+            this.CodeKeyButton.Text = "اظهار المفتاح";
+            this.CodeKeyButton.UseVisualStyleBackColor = false;
+            this.CodeKeyButton.Click += new System.EventHandler(this.CodeKeyButton_Click);
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CopyButton.Enabled = false;
+            this.CopyButton.FlatAppearance.BorderSize = 0;
+            this.CopyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CopyButton.Font = new System.Drawing.Font("Cairo", 10.2F);
+            this.CopyButton.Location = new System.Drawing.Point(271, 572);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(158, 40);
+            this.CopyButton.TabIndex = 5;
+            this.CopyButton.Text = "نسخ التشفير";
+            this.CopyButton.UseVisualStyleBackColor = false;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClearButton.Enabled = false;
+            this.ClearButton.FlatAppearance.BorderSize = 0;
+            this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearButton.Font = new System.Drawing.Font("Cairo", 10.2F);
+            this.ClearButton.Location = new System.Drawing.Point(89, 572);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(158, 40);
+            this.ClearButton.TabIndex = 6;
+            this.ClearButton.Text = "مسح";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // DashesCheckBox
+            // 
+            this.DashesCheckBox.AutoSize = true;
+            this.DashesCheckBox.Checked = true;
+            this.DashesCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.DashesCheckBox.Location = new System.Drawing.Point(398, 624);
+            this.DashesCheckBox.Name = "DashesCheckBox";
+            this.DashesCheckBox.Size = new System.Drawing.Size(78, 21);
+            this.DashesCheckBox.TabIndex = 8;
+            this.DashesCheckBox.Text = "Dashes";
+            this.DashesCheckBox.UseVisualStyleBackColor = true;
+            this.DashesCheckBox.CheckedChanged += new System.EventHandler(this.DashesCheckBox_CheckedChanged);
+            // 
+            // KeysComboBox
+            // 
+            this.KeysComboBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.KeysComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.KeysComboBox.Font = new System.Drawing.Font("Cairo", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KeysComboBox.FormattingEnabled = true;
+            this.KeysComboBox.Location = new System.Drawing.Point(817, 572);
+            this.KeysComboBox.Name = "KeysComboBox";
+            this.KeysComboBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.KeysComboBox.Size = new System.Drawing.Size(158, 40);
+            this.KeysComboBox.TabIndex = 2;
+            this.KeysComboBox.Text = "المفتاح (أ=...)";
+            this.KeysComboBox.SelectedIndexChanged += new System.EventHandler(this.KeysComboBox_SelectedIndexChanged);
+            // 
+            // SlashesCheckBox
+            // 
+            this.SlashesCheckBox.AutoSize = true;
+            this.SlashesCheckBox.Checked = true;
+            this.SlashesCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.SlashesCheckBox.Location = new System.Drawing.Point(480, 624);
+            this.SlashesCheckBox.Name = "SlashesCheckBox";
+            this.SlashesCheckBox.Size = new System.Drawing.Size(80, 21);
+            this.SlashesCheckBox.TabIndex = 9;
+            this.SlashesCheckBox.Text = "Slashes";
+            this.SlashesCheckBox.UseVisualStyleBackColor = true;
+            this.SlashesCheckBox.CheckedChanged += new System.EventHandler(this.SlashesCheckBox_CheckedChanged);
+            // 
+            // CharactersSpacingCheckBox
+            // 
+            this.CharactersSpacingCheckBox.AutoSize = true;
+            this.CharactersSpacingCheckBox.Checked = true;
+            this.CharactersSpacingCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CharactersSpacingCheckBox.Location = new System.Drawing.Point(564, 624);
+            this.CharactersSpacingCheckBox.Name = "CharactersSpacingCheckBox";
+            this.CharactersSpacingCheckBox.Size = new System.Drawing.Size(154, 21);
+            this.CharactersSpacingCheckBox.TabIndex = 10;
+            this.CharactersSpacingCheckBox.Text = "Characters Spacing";
+            this.CharactersSpacingCheckBox.UseVisualStyleBackColor = true;
+            this.CharactersSpacingCheckBox.CheckedChanged += new System.EventHandler(this.CharactersSpacingCheckBox_CheckedChanged);
+            // 
+            // WordsSpacingCheckBox
+            // 
+            this.WordsSpacingCheckBox.AutoSize = true;
+            this.WordsSpacingCheckBox.Checked = true;
+            this.WordsSpacingCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.WordsSpacingCheckBox.Location = new System.Drawing.Point(722, 624);
+            this.WordsSpacingCheckBox.Name = "WordsSpacingCheckBox";
+            this.WordsSpacingCheckBox.Size = new System.Drawing.Size(126, 21);
+            this.WordsSpacingCheckBox.TabIndex = 11;
+            this.WordsSpacingCheckBox.Text = "Words Spacing";
+            this.WordsSpacingCheckBox.UseVisualStyleBackColor = true;
+            this.WordsSpacingCheckBox.CheckedChanged += new System.EventHandler(this.WordsSpacingCheckBox_CheckedChanged);
+            // 
+            // EncodingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1247, 667);
+            this.Controls.Add(this.WordsSpacingCheckBox);
+            this.Controls.Add(this.CharactersSpacingCheckBox);
+            this.Controls.Add(this.SlashesCheckBox);
+            this.Controls.Add(this.KeysComboBox);
+            this.Controls.Add(this.DashesCheckBox);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.CodeKeyButton);
+            this.Controls.Add(this.CodesComboBox);
+            this.Controls.Add(this.EncodeButton);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "EncodingForm";
+            this.Text = "Scouts Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Button EncodeButton;
+        private System.Windows.Forms.ComboBox CodesComboBox;
+        private System.Windows.Forms.Button CodeKeyButton;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.CheckBox DashesCheckBox;
+        private System.Windows.Forms.ComboBox KeysComboBox;
+        private System.Windows.Forms.CheckBox SlashesCheckBox;
+        private System.Windows.Forms.CheckBox CharactersSpacingCheckBox;
+        private System.Windows.Forms.CheckBox WordsSpacingCheckBox;
+    }
+}
+
---
+++ Scouts Encoder/EncodingForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scouts_Encoder
+{
+    public partial class EncodingForm : Form
+    {
+        public EncodingForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private List<string> arabicLetters = new List<string>
+                                             {"ا", "ب", "ت", "ث", "ج", "ح", "خ",
+                                              "د", "ذ", "ر", "ز", "س", "ش", "ص",
+                                              "ض", "ط", "ظ", "ع", "غ", "ف", "ق",
+                                              "ك", "ل", "م", "ن", "ه", "و", "ي"};
+
+        private string[] numericCode = {"١" , "٢" , "٣" , "٤" , "٥" , "٦" , "٧" ,
+                                        "٨" , "٩" , "١٠", "١١", "١٢", "١٣", "١٤",
+                                        "١٥", "١٦", "١٧", "١٨", "١٩", "٢٠", "٢١",
+                                        "٢٢", "٢٣", "٢٤", "٢٥", "٢٦", "٢٧", "٢٨"};
+
+        private string[] jesusCode = {"ي١", "ي٢", "ي٣", "ي٤", "ي٥", "ي٦", "ي٧",
+                                      "س١", "س٢", "س٣", "س٤", "س٥", "س٦", "س٧",
+                                      "و١", "و٢", "و٣", "و٤", "و٥", "و٦", "و٧",
+                                      "ع١", "ع٢", "ع٣", "ع٤", "ع٥", "ع٦", "ع٧"};
+
+        private string[] invertedCode = {"ي", "و", "ه", "ن", "م", "ل", "ك",
+                                         "ق", "ف", "غ", "ع", "ظ", "ط", "ض",
+                                         "ص", "ش", "س", "ز", "ر", "ذ", "د",
+                                         "خ", "ح", "ج", "ث", "ت", "ب", "ا"};
+
+        private string[] morseCode = {"(•-)"  , "(-•••)", "(-)"   , "(-•-•)", "(•---)" , "(••••)", "(---)" ,
+                                      "(-••)" , "(--••)", "(•-•)" , "(---•)", "(•••)"  , "(----)", "(-••-)",
+                                      "(•••-)", "(••-)" , "(-•--)", "(•-•-)", "(--•)"  , "(••-•)", "(--•-)",
+                                      "(-•-)" , "(•-••)", "(--)"  , "(-•)"  , "(••-••)", "(•--)" , "(••)"  };
+        
+        private int codeIndex;
+        private int shiftingKey;
+        private string textCopy;
+        private bool isDashesChecked  = true;
+        private bool isSlashesChecked = true;
+        private bool isCharactersSpacingChecked = true;
+        private bool isWordsSpacingChecked      = true;
+
+        private void replaceOddCharacters()
+        {
+            textCopy = InputTextBox.Text
+                      .Replace("أ", "ا")
+                      .Replace("إ", "ا")
+                      .Replace("آ", "ا")
+                      .Replace("ء", "ا")
+                      .Replace("ة", "ه")
+                      .Replace("ؤ", "و")
+                      .Replace("ى", "ي")
+                      .Replace("ئ", "ي");
+        }
+
+        private void encode(string[] code)
+        {
+            int index;
+            for (int i = 0; i <= textCopy.Length - 1; i++)
+            {
+                index = arabicLetters.IndexOf(textCopy[i].ToString());
+                if (index == -1)
+                {
+                    if (textCopy[i] == ' ')
+                        if (isSlashesChecked && isWordsSpacingChecked)
+                            OutputTextBox.Text += "  /  ";
+                        else if (!isSlashesChecked && !isWordsSpacingChecked)
+                            OutputTextBox.Text += " ";
+                        else if (isSlashesChecked && !isWordsSpacingChecked)
+                            OutputTextBox.Text += " / ";
+                        else if (!isSlashesChecked && isWordsSpacingChecked)
+                            OutputTextBox.Text += "   ";
+                    else
+                        OutputTextBox.Text += textCopy[i];
+                }
+                else
+                {
+                    index += shiftingKey;
+                    if (index >= code.Length)
+                        index -= code.Length;
+                    OutputTextBox.Text += code[index];
+
+                    if (i + 1 <= textCopy.Length - 1) //check if (i + 1) is an existing index
+                        if (textCopy[i + 1] != ' ' && textCopy[i + 1] != '\r')
+                            if (isDashesChecked && isCharactersSpacingChecked)
+                                OutputTextBox.Text += " - ";
+                            else if (!isDashesChecked && !isCharactersSpacingChecked)
+                                OutputTextBox.Text += "";
+                            else if (isDashesChecked && !isCharactersSpacingChecked)
+                                OutputTextBox.Text += "-";
+                            else if (!isDashesChecked && isCharactersSpacingChecked)
+                                OutputTextBox.Text += " ";
+                }
+            }
+        }
+
+        private void showEncodingKey(string[] code)
+        {
+            int index;
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    index = i + j * 7 + shiftingKey;
+                    if (index >= code.Length)
+                        index -= code.Length;
+                    OutputTextBox.Text += (arabicLetters[i + j * 7] + " = " + code[index + 0]).PadRight(13);
+                }
+                OutputTextBox.Text += "\r\n";
+            }       
+        }
+
+        private void EncodeButton_Click(object sender, EventArgs e)
+        {
+            OutputTextBox.Text = "";
+            replaceOddCharacters();
+
+            switch (codeIndex)
+            {
+                case 0:
+                    encode(jesusCode);
+                    break;
+
+                case 1:
+                    encode(numericCode);
+                    break;
+
+                case 2:
+                    encode(invertedCode);
+                    break;
+
+                case 3:
+                    encode(morseCode);
+                    break;
+            }
+
+        }
+
+        private void CodeKeyButton_Click(object sender, EventArgs e)
+        {
+            OutputTextBox.Text = "";
+
+            switch (codeIndex)
+            {
+                case 0:
+                    showEncodingKey(jesusCode);
+                    break;
+
+                case 1:
+                    showEncodingKey(numericCode);
+                    break;
+
+                case 2:
+                    showEncodingKey(invertedCode);
+                    break;
+
+                case 3:
+                    showEncodingKey(morseCode);
+                    OutputTextBox.Text = OutputTextBox.Text.Replace("(", "").Replace(")", "");
+                    break;
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            OutputTextBox.Text = "";
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(OutputTextBox.Text);
+        }
+
+        private void CodesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CodesComboBox.SelectedItem != null)
+            {
+                codeIndex = CodesComboBox.SelectedIndex;
+                EncodeButton .Enabled = true;
+                CodeKeyButton.Enabled = true;
+                CopyButton   .Enabled = true;
+                ClearButton  .Enabled = true;
+                KeysComboBox.Items.Clear();
+
+                switch (codeIndex)
+                {
+                    case 0:
+                        KeysComboBox.Items.AddRange(jesusCode);
+                        break;
+
+                    case 1:
+                        KeysComboBox.Items.AddRange(numericCode);
+                        break;
+
+                    case 2:
+                        KeysComboBox.Items.AddRange(invertedCode);
+                        break;
+
+                    case 3:
+                        KeysComboBox.Items.Add("لا يوجد مفاتيح");
+                        break;
+                }
+            }
+        }
+
+        private void KeysComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            shiftingKey = KeysComboBox.SelectedIndex;
+        }
+
+
+
+
+        private void DashesCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            isDashesChecked = DashesCheckBox.Checked;
+        }
+
+        private void SlashesCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            isSlashesChecked = SlashesCheckBox.Checked;
+        }
+
+        private void CharactersSpacingCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            isCharactersSpacingChecked = CharactersSpacingCheckBox.Checked;
+        }
+
+        private void WordsSpacingCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            isWordsSpacingChecked = WordsSpacingCheckBox.Checked;
+        }
+
+        //Placeholder Text Events
+        private void InputTextBox_Enter(object sender, EventArgs e)
+        {
+            if (InputTextBox.Text == "ادخل الشفرة هنا ...")
+                InputTextBox.Text = "";
+        }
+
+        private void InputTextBox_Leave(object sender, EventArgs e)
+        {
+            if (InputTextBox.Text == "")
+                InputTextBox.Text = "ادخل الشفرة هنا ...";
+        }
+
+    }
+}
