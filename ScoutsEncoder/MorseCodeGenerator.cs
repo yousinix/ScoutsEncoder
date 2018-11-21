49f22941364ca886d93759e7bfb369a01f23ad66
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-11-21 23:56:16 +0200

    Edit MorseCodeGenerator class
     * Remove unnecessary/unused variables.
     * Create a separate function for Save.
     * Code alignment to improve code readability.
     * Edit comments.

--- ScoutsEncoder/MorseCodeGenerator.cs
+++ ScoutsEncoder/MorseCodeGenerator.cs
@@ -1,77 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScoutsEncoder
 {
     public class WaveHeader
     {
-        public string sGroupID;   // RIFF
-        public uint dwFileLength; // total file length minus 8, which is taken up by RIFF
-        public string sRiffType;  // always WAVE
+        public string sGroupID;      // RIFF
+        public uint   dwFileLength;  // total file length minus 8, which is taken up by RIFF
+        public string sRiffType;     // always WAVE
 
         /// <summary>
         /// Initializes a WaveHeader object with the default values.
         /// </summary>
         public WaveHeader()
         {
+            sGroupID     = "RIFF";
             dwFileLength = 0;
-            sGroupID = "RIFF";
-            sRiffType = "WAVE";
+            sRiffType    = "WAVE";
         }
     }
 
     public class WaveFormatChunk
     {
-        public string sChunkID;         // Four bytes: "fmt "
-        public uint dwChunkSize;        // Length of header in bytes
-        public ushort wFormatTag;       // 1 (MS PCM)
-        public ushort wChannels;        // Number of channels
-        public uint dwSamplesPerSec;    // Frequency of the audio in Hz... 44100
-        public uint dwAvgBytesPerSec;   // for estimating RAM allocation
-        public ushort wBlockAlign;      // sample frame size, in bytes
-        public ushort wBitsPerSample;   // bits per sample
+        public string sChunkID;          // Four bytes: "fmt "
+        public uint   dwChunkSize;       // Length of header in bytes
+        public ushort wFormatTag;        // 1 (MS PCM)
+        public ushort wChannels;         // Number of channels (1 for Mono or 2 for Stereo)
+        public uint   dwSamplesPerSec;   // Frequency of the audio in Hz... 44100
+        public uint   dwAvgBytesPerSec;  // for estimating RAM allocation
+        public ushort wBlockAlign;       // sample frame size, in bytes
+        public ushort wBitsPerSample;    // bits per sample
 
         /// <summary>
         /// Initializes a format chunk with the following properties:
         /// Sample rate: 44100 Hz
-        /// Channels: Stereo
+        /// Channels: Mono
         /// Bit depth: 16-bit
         /// </summary>
         public WaveFormatChunk()
         {
-            sChunkID = "fmt ";
-            dwChunkSize = 16;
-            wFormatTag = 1;
-            wChannels = 1;
-            dwSamplesPerSec = 44100;
-            wBitsPerSample = 16;
-            wBlockAlign = (ushort)(wChannels * (wBitsPerSample / 8));
+            sChunkID         = "fmt ";
+            dwChunkSize      = 16;
+            wFormatTag       = 1;
+            wChannels        = 1;
+            dwSamplesPerSec  = 44100;
+            wBitsPerSample   = 16;
+            wBlockAlign      = (ushort)(wChannels * (wBitsPerSample / 8));
             dwAvgBytesPerSec = dwSamplesPerSec * wBlockAlign;
         }
     }
 
     public class WaveDataChunk
     {
-        public string sChunkID;     // "data"
-        public uint dwChunkSize;    // Length of header in bytes
-        public short[] dashArray;   // 8-bit audio
-        public short[] dotArray;    // 8-bit audio
+        public string sChunkID;       // "data"
+        public uint dwChunkSize;      // Length of header in bytes
+        public short[] dotArray;      // 8-bit audio
         public short[] silenceArray;  // 8-bit audio
-
 
         /// <summary>
         /// Initializes a new data chunk with default values.
         /// </summary>
         public WaveDataChunk()
         {
-            dotArray = new short[0];
+            sChunkID     = "data";
+            dwChunkSize  = 0;
+            dotArray     = new short[0];
             silenceArray = new short[0];
-            dwChunkSize = 0;
-            sChunkID = "data";
         }
     }
 
@@ -82,44 +76,37 @@ namespace ScoutsEncoder
         WaveHeader header;
         WaveFormatChunk format;
         WaveDataChunk data;
+        string cipher;
 
-        public MorseCodeGenerator(string morseCode, string filePath)
+        public MorseCodeGenerator(string morseCode)
         {
             // Init chunks
             header = new WaveHeader();
             format = new WaveFormatChunk();
-            data = new WaveDataChunk();
-
-            // Number of samples = sample rate * channels * bytes per sample
-            uint numSamples = format.dwSamplesPerSec * format.wChannels;
+            data   = new WaveDataChunk();
+            cipher = morseCode;
 
             // Initialize the 16-bit array
-            data.dotArray = new short[numSamples / 10];
-            data.silenceArray = new short[numSamples / 10]; //automatically Initialized with zeroes
+            data.dotArray     = new short[format.dwSamplesPerSec / 10];
+            data.silenceArray = new short[format.dwSamplesPerSec / 10]; //automatically Initialized with zeroes
 
-            //Wave Properties
+            // Sine wave Properties
             int amplitude = 32760;  // Max amplitude for 16-bit audio
-            double freq = 750.0f;
+            double freq   = 750.0f;
             double periodOfWave = (Math.PI * 2 * freq) / (format.dwSamplesPerSec);
 
-            for (uint i = 0; i < numSamples / 10; i++)
+
+            // Fill dot array with sine waves
+            for (uint i = 0; i < format.dwSamplesPerSec / 10; i++)
             {
-                //fill dot array with sine waves
                 data.dotArray[i] = Convert.ToInt16(amplitude * Math.Sin(periodOfWave * i));
             }
 
-            // Morse Code:
-            // 1. The duration of a Dash is three times the duration of a dot.
-            // 2. Each dot or dash within a character is followed by period of signal absence,
-            //    called a space, equal to the dot duration.
-            // 3. Letters are separated by a space of duration equal to three dots.
-            // 4. Words are separated by a space of duration equal to seven dots.
-
 
             // Calculate data chunk size in bytes
-            for (int index = 0; index < morseCode.Length; index++)
+            for (int index = 0; index < cipher.Length; index++)
             {
-                switch (morseCode[index])
+                switch (cipher[index])
                 {
                     case '-': // 4 frames
                         data.dwChunkSize += (uint)((data.dotArray.Length * (format.wBitsPerSample / 8)) * 4);
@@ -131,7 +118,6 @@ namespace ScoutsEncoder
 
                     case '.': // 3 frames
                         data.dwChunkSize += (uint)((data.dotArray.Length * (format.wBitsPerSample / 8)) * 3);
-
                         break;
 
                     case ' ': // 7 frames
@@ -142,7 +128,10 @@ namespace ScoutsEncoder
                         continue;
                 }
             }
+        }
 
+        public void Save(string filePath)
+        { 
             // Create a file (it always overwrites)
             FileStream fileStream = new FileStream(filePath, FileMode.Create);
 
@@ -168,9 +157,19 @@ namespace ScoutsEncoder
             writer.Write(data.sChunkID.ToCharArray());
             writer.Write(data.dwChunkSize);
 
-            for (int index = 0; index < morseCode.Length; index++)
+
+            // Morse Code:
+            // 1. The duration of a Dash is three times the duration of a dot.
+            // 2. Each dot or dash within a character is followed by period of signal absence,
+            //    called a space, equal to the dot duration.
+            // 3. Letters are separated by a space of duration equal to three dots.
+            // 4. Words are separated by a space of duration equal to seven dots.
+
+
+            // Write the cipher
+            for (int index = 0; index < cipher.Length; index++)
             {
-                switch (morseCode[index])
+                switch (cipher[index])
                 {
                     case '-': // Dash = 3 dots + period
 
@@ -203,7 +202,6 @@ namespace ScoutsEncoder
                                 writer.Write(dataPoint);
                         }
 
-
                         break;
 
                     case ' ': // Word Space = 7 dots
@@ -228,7 +226,6 @@ namespace ScoutsEncoder
             // Clean up
             writer.Close();
             fileStream.Close();
-
         }
 
     }
