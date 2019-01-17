1bda3ba9e74d9b51edf21215d06f570fc727068b
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-01-17 16:43:00 +0200

    Add Clipboard Buttons' Event Handlers

--- ScoutsEncoder_WPF/MainWindow.xaml.cs
+++ ScoutsEncoder_WPF/MainWindow.xaml.cs
@@ -15,6 +15,68 @@ namespace ScoutsEncoder_WPF
             InitializeComponent();
         }
 
+
+
+
+        //// Input Event Handlers ////
+
+        private void InputCutButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputTextBox.Text != "")
+            {
+                Clipboard.SetText(InputTextBox.Text);
+            }
+            InputTextBox.Text = "";
+        }
+
+        private void InputCopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputTextBox.Text != "")
+            {
+                Clipboard.SetText(InputTextBox.Text);
+            }
+        }
+
+        private void InputPasteButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text += Clipboard.GetText();
+        }
+
+        private void InputClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextBox.Text = "";
+        }
+
+
+
+
+        //// Output Event Handlers ////
+
+        private void OutputCutButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (OutputTextBox.Text != "")
+            {
+                Clipboard.SetText(OutputTextBox.Text);
+            }
+            OutputTextBox.Text = "";
+        }
+
+        private void OutputCopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (OutputTextBox.Text != "")
+            {
+                Clipboard.SetText(OutputTextBox.Text);
+            }
+        }
+
+        private void OutputClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            OutputTextBox.Text = "";
+        }
+
+
+
+
         //// Footer's Event Handlers ////
 
         string currentTheme = "Light"; // Default theme
