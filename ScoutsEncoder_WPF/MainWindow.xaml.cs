585623f16fee451baf89d8aa66939f76f9f9bd17
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-01-17 10:51:09 +0200

    Add click event handlers to Footer's buttons

--- ScoutsEncoder_WPF/MainWindow.xaml.cs
+++ ScoutsEncoder_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using MaterialDesignThemes.Wpf;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Navigation;
 
@@ -12,6 +13,48 @@ namespace ScoutsEncoder_WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //// Footer's Event Handlers ////
+
+        string currentTheme = "Light"; // Default theme
+        private void ToggleThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentTheme == "Light")
+            {
+                ThemeAssist.SetTheme(this, BaseTheme.Dark);
+                currentTheme = "Dark";
+            }
+            else if (currentTheme == "Dark")
+            {
+                ThemeAssist.SetTheme(this, BaseTheme.Light);
+                currentTheme = "Light";
+            }
+        }
+
+        private void GitHubButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/YoussefRaafatNasry/ScoutsEncoder/");
+        }
+
+        private void GoogleDocsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://doc.new/");
+        }
+
+        private void WebsiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://youssefraafatnasry.github.io/ScoutsEncoder/");
+        }
+
+        private void ScoutsEncoderDocsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://youssefraafatnasry.github.io/ScoutsEncoder/docs/all/");
+        }
+
+        private void BugReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("mailto:YoussefRaafatNasry@gmail.com?subject=ScoutsEncoder%20|%20Bug%20Report");
         }
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
