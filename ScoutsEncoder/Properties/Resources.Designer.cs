a3b898463e3437ad0a1e23bb3734c0d7596b11e2
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-01-19 13:22:45 +0200

    Update & Embed Icon

--- ScoutsEncoder/Properties/Resources.Designer.cs
+++ ScoutsEncoder/Properties/Resources.Designer.cs
@@ -61,12 +61,12 @@ namespace ScoutsEncoder.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
-        internal static System.Drawing.Bitmap logo {
+        internal static System.Drawing.Icon logo {
             get {
                 object obj = ResourceManager.GetObject("logo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
+                return ((System.Drawing.Icon)(obj));
             }
         }
     }
