36296dfe7660156583365ebb7d41fa28c4b1256a
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-08-15 02:11:08 +0200

    Edit Assembly Info

--- ScoutsEncoder/Properties/AssemblyInfo.cs
+++ ScoutsEncoder/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScoutsEncoder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Encode plain text into scouts' ciphers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("YoussefRaafatNasry")]
 [assembly: AssemblyProduct("ScoutsEncoder")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
