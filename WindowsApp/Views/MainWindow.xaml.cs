dc81a72ef69c5e4a7ec7b868c1f5307c9a9935c4
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2020-05-03 16:31:25 +0200

    Increase Mirror Selection Accuracy

--- WindowsApp/Views/MainWindow.xaml.cs
+++ WindowsApp/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
 using WindowsApp.ViewModels;
@@ -17,22 +18,32 @@ namespace WindowsApp.Views
         private void MirrorSelectionEventHandler(object sender, RoutedEventArgs e)
         {
             // Clear old mirror
-            var baseStartPtr   = OutputRichTextBox.Document.ContentStart;
-            var baseEndPtr     = OutputRichTextBox.Document.ContentEnd;
-            var baseRange      = new TextRange(baseStartPtr, baseEndPtr);
+            var baseStartPtr = OutputRichTextBox.Document.ContentStart;
+            var baseEndPtr   = OutputRichTextBox.Document.ContentEnd;
+            var baseRange    = new TextRange(baseStartPtr, baseEndPtr);
             baseRange.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Transparent);
 
             if (InputTextBox.SelectionLength == 0) return;
 
             // Get mirror target info
-            var selectedText   = Cipher.Encode(InputTextBox.SelectedText);
-            var precedingText  = Cipher.Encode(InputTextBox.Text.Substring(0, InputTextBox.SelectionStart));
-            var mirrorStart    = precedingText.Length + 2;
-            var mirrorLength   = selectedText .Length + 2;
+            var precedingText = InputTextBox.Text.Substring(0, InputTextBox.SelectionStart);
+            var selectedText  = InputTextBox.SelectedText;
+
+            var precedingEncoding = Cipher.Encode(precedingText);
+            var selectedEncoding  = Cipher.Encode(selectedText);
+
+            var charDelimiterOffset = Cipher.CharsDelimiter.Length;
+            var precedingTextOffset = precedingText.Length == 0 || char.IsWhiteSpace(precedingText.Last()) ? 0 : charDelimiterOffset;
+            var selectedTextOffset  = char.IsWhiteSpace(selectedText.Last()) ? 0 : charDelimiterOffset;
+
+            var mirrorStart  = precedingEncoding.Length + precedingTextOffset;
+            var mirrorLength = selectedEncoding .Length + selectedTextOffset;
 
             // Apply mirror to target
-            var mirrorStartPtr = baseStartPtr.GetPositionAtOffset(mirrorStart);
-            var mirrorEndPtr   = mirrorStartPtr?.GetPositionAtOffset(mirrorLength) ?? baseEndPtr;
+            var targetStartPtr = OutputRun.ContentStart;
+            var targetEndPtr   = OutputRun.ContentEnd;
+            var mirrorStartPtr = targetStartPtr .GetPositionAtOffset(mirrorStart);
+            var mirrorEndPtr   = mirrorStartPtr?.GetPositionAtOffset(mirrorLength) ?? targetEndPtr;
             var mirrorRange    = new TextRange(mirrorStartPtr, mirrorEndPtr);
             mirrorRange.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.CornflowerBlue);
         }
